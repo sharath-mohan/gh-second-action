5fd4341c55ac8dfdaf82918d6da288bca09486aa
sharath-mohan <sharathdesignsofficial@gmail.com>
2025-09-27 14:51:51 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts", // Or wherever the setup file is located
+  },
+});
